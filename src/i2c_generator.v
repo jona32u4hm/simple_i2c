6d9b694f36cb1fa31684386a508954ec66594859
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-06-04 16:47:09 -0600

    states up to ACK complete

--- src/i2c_generator.v
+++ src/i2c_generator.v
@@ -19,22 +19,25 @@ module i2c_generator (
 
 
     reg _nxt_sda_o;
-    reg [1:0] _stage_count, _nxt_stage_count;
+    reg [4:0] _stage_count, _nxt_stage_count;
+    reg [7:0] _shift, _shifted;
 
   
     reg [:0] _state, _next_state;
     localparam  IDLE        = 
                 START       = 
                 ADDR        = 
+                ACK         =
 
     always @(*) begin
+        SDA_OE = 1;
         _next_state = _state;
         _scl = 1;
-        _nxt_sda_o = 1;  
+        _nxt_sda_o = SDA_OUT;  
+        _shifted = _shift;
         case (_state)
             IDLE: begin
                 _nxt_stage_count = 2'b11;
-                SDA_OE = 1;
                 _nxt_sda_o = 1;    
                 if (start_stb_i) begin
                     _next_state = START;
@@ -44,9 +47,30 @@ module i2c_generator (
             START: begin
                 _nxt_stage_count = _stage_count +1;
                 _nxt_sda_o = 0;  
+                _next_state = ADDR;
+                _shifted = I2C_ADDR;
             end
             ADDR: begin
-                
+                _nxt_stage_count = _stage_count +1;
+                if (_stage_count[1:0] == 2'b00)begin
+                    _nxt_sda_o = _shift[7];
+                    _shifted = {shift_reg[6:0], 1'b1};
+                end
+                if (_stage_count[4:2] == 3'b111)begin
+                    _next_state = ACK;
+                end
+            end
+            ACK: begin
+                _nxt_stage_count = _stage_count +1;
+                if (_stage_count != 0) begin
+                    SDA_OE = 0;
+                    if (_stage_count[1:0] == 2'b10 && SDA_IN == 0) begin //if NACK
+                        _next_state = IDLE; 
+                    end 
+                    if (_stage_count[2] == 1'b1) begin
+                        _next_state = DATA
+                    end
+                end
             end
         endcase
     end
@@ -61,6 +85,7 @@ module i2c_generator (
             SDA_OUT <= _nxt_sda_o;
             SCL <= _nxt_stage_count[1];
             _stage_count <= _nxt_stage_count;
+            _shift <= _shifted;
         end
     end
 
