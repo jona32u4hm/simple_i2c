7059efaf10679bdff9e7e7961c8d3bb978ac35db
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-06-04 14:13:38 -0600

    reduced design to one file because of simplicity

--- src/i2c_generator.v
+++ src/i2c_generator.v
@@ -1,52 +1,60 @@
 module i2c_generator (
     // Reloj y Reinicio
-    input  wire        CLK,
-    input  wire        RST,      // RST=1 para funcionamiento normal, 0 para reinicio
+    input          CLK,
+    input          RST,      // RST=1 para funcionamiento normal, 0 para reinicio
 
     // Interfaz con el CPU
-    input  wire        RNW,
-    input  wire [6:0]  I2C_ADDR,
-    input  wire [15:0] WR_DATA, //write data from cpu to I2C
-    output wire [15:0] RD_DATA, //read data from I2C to cpu
-    input  wire        START_STB,
+    input          RNW,
+    input   [6:0]  I2C_ADDR,
+    input   [15:0] WR_DATA, //write data from cpu to I2C
+    output  [15:0] RD_DATA, //read data from I2C to cpu
+    input          START_STB,
 
     // Interfaz Física I2C / Probador
-    output wire        SCL,
-    output wire        SDA_OUT,
-    output wire        SDA_OE,
-    input  wire        SDA_IN
+    output reg        SCL,
+    output reg        SDA_OUT,
+    output reg        SDA_OE,
+    input         SDA_IN
 );
 
 
+    reg _nxt_sda_o;
+
+  
+    reg [:0] _state, _next_state;
+    localparam  IDLE        = 
+                START       = 
+                ADDR        = 
+
+    always @(*) begin
+        _next_state = _state;
+        case (_state)
+            IDLE: begin
+                SDA_OE = 1;
+                _nxt_sda_o = 1;    
+                if (start_stb_i) begin
+                    _next_state = SHIFT;
+                    _nxt_sda_o = 0;
+                end
+            end
+            START: begin
+
+            end
+            ADDR: begin
+                
+            end
+        endcase
+    end
 
 
-
-i2ctx u_i2ctx (
-    .clk_i(CLK),
-    .rst_i(RST),
-    .shift_i(), // Shift durante SCL bajo
-    .write_i(), // Solo se transmite el byte menos significativo
-    .srtb_i(START_STB), // Carga el shift register al iniciar la transmisión
-    .sda_o(SDA_OUT)
-);
-
-i2crx u_i2crx (
-    .clk_i(CLK),
-    .rst_i(RST),
-    .read_o(), // Solo se recibe el byte menos significativo
-    .sample_i(), // Shift durante SCL alto
-    .sda_i(SDA_IN)
-);
-
-
-
-
-wire        _scl_en;    
-clk_divider u_clk_divider_i2c (
-    .clk_i  (CLK),    
-    .rst_i  (RST),  
-    .scl_o  (SCL),    
-    .scl_en (_scl_en)  
-);
+    always @(posedge CLK or negedge RST) begin
+        if (!RST) begin
+            //reset
+        end else begin
+            // not a reset
+            _state <= _next_state;
+            SDA_OUT <= _nxt_sda_o;
+        end
+    end
 
 endmodule
